b28a337e79d57d12dec95db67877a61cee8010f5
juanocasio <juanocasio@comcast.net>
2016-05-23 22:00:45 -0400

    first commit

---
+++ components/classifiedsController.js
@@ -0,0 +1,9 @@
+(function () {
+    "use strict";
+
+    angular
+        .module("ngClassifieds")
+        .controller("ClassifiedsCtrl", function ($scope) {
+            $scope.name = 'Juan Ocasio';
+        });
+})();
